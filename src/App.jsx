8e598eafd9c98d80cc6598bfc2ab2a0926ff5616
Stanislav2022 <tsv5@ukr.net>
2023-03-03 17:08:12 +0200

    1

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import Phonebook from "./components/Phonebook/Phonebook";
 
 export const App = () => {
   return (
-    <div>
-      <Phonebook/>
-    </div>
+       <div>
+          <Phonebook/>
+      </div>
   );
 };
--- src/components/Phonebook/ContactForm/ContactForm.jsx
+++ src/components/Phonebook/ContactForm/ContactForm.jsx
@@ -67,67 +67,6 @@ export default function ContactForm({ onSubmit }) {
 }
 
 
-// export default class ContactForm extends Component {
-//     state = {
-//         name: '',
-//         number: '',
-//     };
-    
-//     handlePhonebook = (e) => {
-//         const { name, value } = e.target
-//         this.setState({ [name]: value });
-        
-//     };
-
-//     nameId = nanoid();
-//     numberId = nanoid();
-
-//     handleSubmite = (e) => {
-//         e.preventDefault()
-//         const { name, number } = this.state;
-//         this.props.onSubmit({ name, number });
-//         this.setState({
-//             name: '',
-//             number: '',
-//         });
-//     };    
-
-//     render() {
-//         const { nameId, numberId, handleSubmite } = this;
-//     return (
-//         <form onSubmit={handleSubmite}>
-//             <div>
-//                 <label htmlFor={nameId}> Name </label>
-//                 <input
-//                     className={css.input}
-//                     id={nameId}
-//                     type="text"
-//                     name="name"
-//                     value={this.state.name}
-//                     pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-//                     title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-//                     required onChange={this.handlePhonebook} />
-//             </div>
-//             <div>
-//                 <label htmlFor={numberId} > Number </label>
-//                 <input
-//                     className={css.input}
-//                     id={numberId}
-//                     type="tel"
-//                     name="number"
-//                     value={this.state.number}
-//                     pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-//                     title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-//                     required onChange={this.handlePhonebook} />
-//             </div>
-//             <button>Add contact</button>
-//                 </form>
-     
-
-
-//     )
-//   }
-// }
 
 ContactForm.propTypes = {
   onSubmit: PropTypes.func.isRequired,
